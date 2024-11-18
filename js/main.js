db50f963a91ef1e72234b55fef432de2b85eb0d5
HenkSeed <adaner@inbox.ru>
2024-11-18 14:13:49 +0700

    Updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import replaceSVGImages from "./replaceSVGImages.js";
 import redButtonWidth from "./components/redButtonWidth.js";
+import placeholderAsteriskRed from "./components/placeholder_asterisk_red.js";
 
 replaceSVGImages();
 redButtonWidth();
+// placeholderAsteriskRed();
