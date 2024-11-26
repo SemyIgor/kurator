d270aff08a62cc3fe3780af591a8890e0542fc41
HenkSeed <adaner@inbox.ru>
2024-11-27 00:15:59 +0700

    Updates

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ import redButtonWidth from "./components/redButtonWidth.js";
 import shadow from "./components/shadow.js";
 import shadowBottom from "./components/shadow_bottom.js";
 import shadowTop from "./components/shadow-top.js";
+import shadowFade from "./components/shadow_fade.js";
 
 replaceSVGImages();
 redButtonWidth();
 shadow();
 shadowBottom();
 shadowTop();
+shadowFade();
